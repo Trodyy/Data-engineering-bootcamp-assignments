9c06a976b4a71f9279d5b6683ba174a3a650463b
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-07-21 01:41:20 +0330

    fix : add UPSERT part.
    BREAKING CHANGES : use UPSERT to fix duplicate error!

--- 1-dimensional-data-modeling/answers.sql
+++ 1-dimensional-data-modeling/answers.sql
@@ -23,48 +23,52 @@ CREATE TABLE actors (
 
 WITH yesterday AS (
     SELECT * FROM actors 
-    WHERE current_year = 1969
+    WHERE current_year = 1978
 ),
-today AS (
+
+today_aggregated AS (
     SELECT
-        actor,
-        actorid,
+        actor ,
+		actorid ,
         year,
         ARRAY_AGG(ROW(film, votes, rating, filmid)::films) AS films,
-        AVG(rating) AS avg_rating  
+        AVG(rating) AS avg_rating
     FROM actor_films
-    WHERE year = 1970
+    WHERE year = 1979
     GROUP BY actorid, actor, year
-    ORDER BY actorid
+),
+combined AS (
+    SELECT 
+        COALESCE(y.actor, t.actor) AS actor,
+		COALESCE(y.actorid, t.actorid) AS actorid,
+        COALESCE(t.year, y.current_year + 1) AS current_year,
+        COALESCE(y.films, ARRAY[]::films[]) || 
+        COALESCE(t.films, ARRAY[]::films[]) AS films,
+        CASE
+            WHEN t.actorid IS NOT NULL THEN
+                CASE 
+                    WHEN t.avg_rating > 8 THEN 'star'::quality_class
+                    WHEN t.avg_rating > 7 THEN 'good'::quality_class
+                    WHEN t.avg_rating > 6 THEN 'average'::quality_class
+                    ELSE 'bad'::quality_class
+                END
+            ELSE y.quality_class
+        END AS quality_class,
+        t.actorid IS NOT NULL AS is_active
+    FROM yesterday y
+    FULL OUTER JOIN today_aggregated t
+        ON t.actorid = y.actorid
 )
 
+-- Upsert operation
 INSERT INTO actors
-SELECT 
-    COALESCE(y.actor, t.actor) AS actor,
-    COALESCE(y.actorid, t.actorid) AS actorid,
-	COALESCE(t.year, y.current_year + 1) AS current_year ,
-    COALESCE(y.films, ARRAY[]::films[])
-    || CASE WHEN t.films IS NOT NULL
-        THEN t.films
-        ELSE ARRAY[]::films[] END AS films,
-    (CASE 
-        WHEN t.avg_rating > 8 THEN 'star'
-        WHEN t.avg_rating > 7 THEN 'good'
-        WHEN t.avg_rating > 6 THEN 'average'
-        ELSE 'bad'
-    END)::quality_class AS quality_class,
-    t.actorid IS NOT NULL AS is_active
-    
-FROM yesterday y
-FULL OUTER JOIN today t
-    ON t.actorid = y.actorid;
-
-
-
-
-
-
-
+SELECT * FROM combined
+ON CONFLICT (actorid, actor) DO UPDATE
+SET
+    films = EXCLUDED.films,
+    quality_class = EXCLUDED.quality_class,
+    is_active = EXCLUDED.is_active,
+    current_year = EXCLUDED.current_year;
 
 
 

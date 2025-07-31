e8f9a00dba0419df1ff225b5f917d076c067f783
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-07-31 15:25:50 +0330

    FEAT : Add part 3.
    BREAKING CHANGES : Use cumulated algorithm to track user_activity by browser_type.both devices and events table have dupliactes and we deduplicate them by ROW_NUMBER()

--- 2-fact-data-modeling/answers.sql
+++ 2-fact-data-modeling/answers.sql
@@ -26,3 +26,60 @@ CREATE TABLE user_devices_cumulated (
 
 
 
+--###PART 3 ###--
+WITH yesterday AS (
+	SELECT * FROM user_devices_cumulated
+	WHERE date = DATE('2022-12-31')
+) ,
+	events_aggregated AS (
+		SELECT * ,
+		ROW_NUMBER() OVER (PARTITION BY user_id , event_time) AS row_num
+		FROM events
+) ,
+	events_deduped AS (
+		SELECT * FROM events_aggregated WHERE row_num = 1
+	) ,
+
+	device_aggregated AS (
+		SELECT * ,
+		ROW_NUMBER() OVER (PARTITION BY device_id , browser_type) AS row_num
+		FROM devices
+) ,
+	device_deduped AS (
+		SELECT * FROM device_aggregated WHERE row_num = 1
+	) ,
+	today AS (
+		SELECT 
+			user_id ,
+			dd.browser_type ,
+			DATE(event_time) AS event_time
+		FROM events_deduped ed
+		JOIN device_deduped dd ON ed.device_id = dd.device_id
+		WHERE DATE(event_time) = DATE('2023-01-01')
+		AND user_id IS NOT NULL
+		GROUP BY (user_id , dd.browser_type , DATE(event_time))
+)
+
+
+SELECT
+	COALESCE(t.user_id , y.user_id) AS user_id ,
+	COALESCE(DATE(t.event_time) , y.date + INTERVAL '1 day') AS date ,
+	t.browser_type AS browser_type ,
+	-- ROW_NUMBER() OVER (PARTITION BY t.user_id , t.browser_type ,date)
+	CASE
+		WHEN
+			y.device_activity_datelist IS NOT NULL
+			THEN (y.device_activity_datelist || ARRAY[t.event_time])::DATE[]
+		WHEN t.user_id IS NULL
+			THEN y.device_activity_datelist
+		ELSE
+			ARRAY[t.event_time]::DATE[]
+		END AS device_activity_datelist
+FROM 
+today t FULL OUTER JOIN yesterday y
+ON t.user_id = y.user_id
+ORDER BY user_id
+-- SELECT * FROM events WHERE DATE(event_time) = DATE('2023-01-01') AND user_id = '16527802169428800000'
+-- SELECT user_id,device_id FROM events
+-- WHERE DATE(event_time) = DATE('2023-01-01')
+-- GROUP BY (user_id , device_id)

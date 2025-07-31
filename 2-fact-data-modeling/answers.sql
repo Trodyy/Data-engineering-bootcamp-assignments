6a776c3831cea45bdedf6ff7141544c598fddb53
trody <mahdi.karimi.beiranvand2003@gmail.com>
2025-07-31 12:32:31 +0330

    FEAT : ADD part 2.

--- 2-fact-data-modeling/answers.sql
+++ 2-fact-data-modeling/answers.sql
@@ -7,3 +7,22 @@ FROM game_details
 )
 SELECT * FROM deduped
 WHERE row_num = 1
+
+
+
+
+
+
+--###PART 2 ###--
+CREATE TABLE user_devices_cumulated (
+	user_id TEXT ,
+	date DATE ,
+	browser_type TEXT ,
+	device_activity_datelist DATE ,
+	PRIMARY KEY (user_id , browser_type)
+)
+
+
+
+
+
